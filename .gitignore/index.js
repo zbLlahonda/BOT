de0591a19034618a6f78c6c0961006266d459f9f
zbLlahonda <42487774+zbLlahonda@users.noreply.github.com>
2018-08-18 09:32:03 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ bot.on('ready', function() {
     console.log("Connectedç");
 });
 
-bot.login(process.env.NDgwMjE3NzM2OTY2OTYzMjEy.DllUtw.P0W5HCnAjiFnOK9ZFxOVFPc47tE);
+bot.login(process.env.NDgwMjE3NzM2OTY2OTYzMjEy.Dllcvw.buu38b_Z3XnLNu6CasbNWi-wMLc);
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
